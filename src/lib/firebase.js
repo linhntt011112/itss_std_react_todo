8860fb42253699014ed72dd5d0b404133ed5f279
EC2 Default User <ec2-user@ip-172-31-62-218.ec2.internal>
2021-05-20 08:29:14 +0000

    Login by Aunthentication

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,16 +1,18 @@
 import firebase from 'firebase';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAYL7QVfrR0OknAOsDOqQZ0UQ07SFfO2jo",
-  authDomain: "fir-sample-5cac5.firebaseapp.com",
-  projectId: "fir-sample-5cac5",
-  storageBucket: "fir-sample-5cac5.appspot.com",
-  messagingSenderId: "757931221815",
-  appId: "1:757931221815:web:fcad29d714555dc2d970b2"
-};
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAYL7QVfrR0OknAOsDOqQZ0UQ07SFfO2jo",
+      authDomain: "fir-sample-5cac5.firebaseapp.com",
+      projectId: "fir-sample-5cac5",
+      storageBucket: "fir-sample-5cac5.appspot.com",
+      messagingSenderId: "757931221815",
+      appId: "1:757931221815:web:fcad29d714555dc2d970b2"
+    };
+    firebase.initializeApp(firebaseConfig);
+}
 const db = firebase.firestore();
 export default firebase;
+export const auth = firebase.auth();
 
 export const getFirebaseItems = async () => {
     try {
@@ -47,9 +49,67 @@ export const updateFirebaseItem = async (item, id) => {
 
 export const clearFirebaseItem = async (item) => {
     const todoRef = db.collection("todos").doc(item.id);
-    await todoRef.delete().then(function () {
+    // await todoRef.delete().then(function () {
 
-    }).catch(function (err) {
-        console.log(err);
+    // }).catch(function (err) {
+    //     console.log(err);
+    await todoRef
+    .delete()
+    .then(function () {})
+    .catch(function (err) {
+      console.log(err);
     });
 }; 
+export const uiConfig = {
+  signInFlow: "popup",
+  signInSuccessUrl: "/",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+};
+
+export const storeUserInfo = async (user) => {
+  const { uid } = user;
+  const userDoc = await db.collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await db.collection("users").doc(uid).set({ name: user.displayName });
+    return {
+      name: user.displayName,
+      id: uid,
+    };
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+    };
+  }
+};
+
+export const updateUser = async (user, image) => {
+  try {
+    const userDoc = await firebase
+      .firestore()
+      .collection("users")
+      .doc(user.id)
+      .get();
+    if (userDoc.exists) {
+      await firebase
+        .firestore()
+        .collection("users")
+        .doc(user.id)
+        .update({ ...userDoc.data(), image: image });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const uploadImage = async (image) => {
+  const ref = firebase.storage().ref().child(`/images/${image.name}`);
+  let downloadUrl = "";
+  try {
+    await ref.put(image);
+    downloadUrl = await ref.getDownloadURL();
+  } catch (err) {
+    console.log(err);
+  }
+  return downloadUrl;
+};
